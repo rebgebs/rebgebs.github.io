12ef2c39da3f2a9ebb5f9230f8df6b2c6384a3c8
rebgebs <rebgebs@byui.edu>
2021-02-18 16:19:15 -0800

    commit

--- assignments/wk07.1/js/lazy.js
+++ assignments/wk07.1/js/lazy.js
@@ -28,15 +28,3 @@ const imgObserver = new IntersectionObserver((entries, imgObserver) => {
 images.forEach(image => {
     imgObserver.observe(image);
 });
-
-// let imagesToLoad = document.querySelectorAll('img[data-src]');
-// const loadImages = (image) => {
-//     image.setAttribute('src', image.getAttribute('data-src'));
-//     image.onload = () => {
-//         image.removeAttribute('data-src');
-//     };
-// };
-// imagesToLoad.forEach((img) => {
-//     loadImages(img);
-// });
-
