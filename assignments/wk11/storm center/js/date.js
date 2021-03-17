7de58cfc1e7bd84c819a9dd2acad0b59b018c03b
rebgebs <rebgebs@byui.edu>
2021-03-17 14:15:57 -0700

    commit

---
+++ assignments/wk11/storm center/js/date.js
@@ -0,0 +1,37 @@
+const daynames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
+const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
+const d = new Date();
+const dayName = daynames[d.getDay()];
+const monthName = months[d.getMonth()];
+const year = d.getFullYear();
+//const fulldate = dayName + ", " + monthName + " " + d.getDate() +", " + year;
+const fulldate = `${dayName}, ${d.getDate()} ${monthName} ${year}`;
+document.getElementById("currentdate").textContent = fulldate;
+
+// let thedate = new Date();
+// thedate.getDay()
+var banner = document.getElementById("banner")
+if (new Date().getDay() != 5) {
+    banner.style.display = "none";
+}
