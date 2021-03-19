12ba6207a47f34dd0c77aa4a64bf1f866bbbea9f
rebgebs <rebgebs@byui.edu>
2021-03-18 21:36:03 -0700

    commit

---
+++ assignments/wk11/fishhaven/js/forecastapi.js
@@ -0,0 +1,42 @@
+const fapiURL = 'https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=b13d7d422b80f4f95da3b17a23b44798';
+
+fetch(fapiURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
+
+
+
+        console.log(jsObject);
+
+
+        var i = 1;
+
+        for (x = 0; x < jsObject.list.length; x++) {
+
+            if (jsObject.list[x].dt_txt.includes("18:00:00")) {
+
+
+                var date = "dayofweek" + i;
+                var temp = "forecast" + i;
+                var icon = "ficon" + i;
+                const imagesrc = 'https://openweathermap.org/img/wn/' + jsObject.list[x].weather[0].icon + '@2x.png';
+                const desc = jsObject.list[x].weather[0].description;
+
+
+                document.getElementById(temp).textContent = Math.round(jsObject.list[x].main.temp);
+                document.getElementById(icon).setAttribute('src', imagesrc);
+                document.getElementById(icon).setAttribute('alt', desc);
+
+                let d = new Date(jsObject.list[x].dt_txt);
+                const dayofWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
+                document.getElementById(date).textContent = dayofWeek[d.getDay()];
+                i++;
+            }
+
+
+        }
+    }
+    );
+
+
---
+++ assignments/wk11/sodasprings/js/weatherapi.js
@@ -0,0 +1,35 @@
+const apiURL = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=b13d7d422b80f4f95da3b17a23b44798';
+
+fetch(apiURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        console.log(jsObject);
+
+
+
+        function windchill() {
+
+            let t = parseFloat(jsObject.main.temp);
+            let s = parseFloat(jsObject.wind.speed);
+
+            if (t < 50 && s > 3) {
+
+                var f = parseInt(35.74 + (0.6215 * t) - (35.75 * (s ** 0.16)) + (0.4275 * t * (s ** 0.16)));
+            }
+            else {
+                var f = "N/A";
+            }
+            return f;
+        }
+
+        document.getElementById('current').innerHTML = jsObject.weather[0].description;
+        document.getElementById('current-temp').innerHTML = jsObject.main.temp;
+        document.getElementById('wind').innerHTML = jsObject.wind.speed;
+        document.getElementById('humidity').innerHTML = jsObject.main.humidity;
+        document.getElementById("chill").innerHTML = windchill();
+    });
+
+
+
+
+
